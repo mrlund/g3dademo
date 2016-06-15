cd4cc432411c0eb59ad7a31644e146391ce2697d
Pavel Ropakov <pavel.ropakov@cogniteq.com>
2016-06-15 14:47:28 +0300

    Directive functionality was improved.

--- app/pages/content-page/inner-content.ts
+++ app/pages/content-page/inner-content.ts
@@ -1,20 +1,23 @@
-import { Component, Input, DynamicComponentLoader,ElementRef,Inject,OnInit} from '@angular/core';
+import { Component, View, Input, DynamicComponentLoader, ElementRef,Injector, OnInit, OnChanges} from '@angular/core';
 
 @Component({
     selector: 'inner-content',
-    template: ' <div class="content-page"></div>'
+    template: '<div id="container"></div>'
 })
-
 export class InnerContent {
+    test: string;
     @Input()
-        template:string;
+        template: string;
 
-    fakeData:string;
+    constructor(private loader: DynamicComponentLoader, private injector:Injector, private elementRef: ElementRef) {
+        this.test = '123';
+    }
 
-    constructor(private dynamicComponentLoader:DynamicComponentLoader, private elementRef:ElementRef) {
-        @Component({selector: 'compiled-component', template: this.template})
-        class CompiledComponent {
-        };
-        this.dynamicComponentLoader.loadAsRoot(CompiledComponent, this.elementRef, 'container');
+    ngOnChanges() {
+        var template = this.template;
+
+        @Component({ selector: 'compiled-component', template: template})
+        class CompiledComponent {};
+        this.loader.loadAsRoot(CompiledComponent, "#container", this.injector);
     }
 }
