61ba7ce7a0351f545df34ff287dd4da936936c31
Pavel Ropakov <pavel.ropakov@cogniteq.com>
2016-07-01 11:01:27 +0300

    Dynamic template loading basic implementation was added.

--- app/pages/content-page/inner-content.ts
+++ app/pages/content-page/inner-content.ts
@@ -1,23 +1,30 @@
-import { Component, View, Input, DynamicComponentLoader, ElementRef,Injector, OnInit, OnChanges} from '@angular/core';
+import { Component, Input, DynamicComponentLoader, ElementRef,Injector, OnInit, OnChanges, ViewChild, ViewContainerRef, ComponentRef} from '@angular/core';
 
 @Component({
     selector: 'inner-content',
-    template: '<div id="container"></div>'
+    template: '<div #container></div>'
 })
 export class InnerContent {
-    test: string;
     @Input()
         template: string;
 
-    constructor(private loader: DynamicComponentLoader, private injector:Injector, private elementRef: ElementRef) {
-        this.test = '123';
+    @ViewChild('container', {read: ViewContainerRef}) target;
+
+    constructor(private dcl: DynamicComponentLoader, private elementRef: ElementRef) {
     }
 
     ngOnChanges() {
         var template = this.template;
-
-        @Component({ selector: 'compiled-component', template: template})
-        class CompiledComponent {};
-        this.loader.loadAsRoot(CompiledComponent, "#container", this.injector);
+        if(template){
+            @Component({
+                selector: 'compiled-component',
+                template: template
+            })
+            class CompiledComponent {}
+            this.target.clear();
+            this.dcl.loadNextToLocation(CompiledComponent, this.target).then((ref:ComponentRef<Type>) => {
+                ref.instance.test = '123';
+            });
+        }
     }
 }
